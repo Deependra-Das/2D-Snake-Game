497c4d6f9828695ea28c89c13532895cbd6fd21c
Deependra-Das <iamdeep75@gmail.com>
2024-08-30 02:59:44 +0530

    Implemented Snake Collision with Food

--- Assets/Scripts/SnakeController.cs
+++ Assets/Scripts/SnakeController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.EditorTools;
 using UnityEngine;
 
 public class SnakeController : MonoBehaviour
@@ -11,12 +12,21 @@ public class SnakeController : MonoBehaviour
     private float moveTimer;
     private float moveTimerMax;
 
+    private List<Transform> snakeSegmentList;
+    public Transform snakeSegmentPrefab;
+
     private void Awake()
     {
         //moveDirection=Vector2.right;
         moveSpeed = 1.0f;
         moveDistance = 1.0f;
         moveTimerMax = 0.1f;
+    }
+
+    private void Start()
+    {
+        snakeSegmentList = new List<Transform>();
+        snakeSegmentList.Add(this.transform);
     }
 
     private void Update()
@@ -44,6 +54,11 @@ public class SnakeController : MonoBehaviour
         moveTimer += Time.fixedDeltaTime;
         if (moveTimer >= moveTimerMax)
         {
+            for (int i = snakeSegmentList.Count-1; i > 0; i--)
+            {
+                snakeSegmentList[i].position = snakeSegmentList[i-1].position;
+            }
+
             this.transform.position = new Vector3(
             Mathf.Round(this.transform.position.x) + moveDirection.x,
             Mathf.Round(this.transform.position.y) + moveDirection.y,
@@ -51,6 +66,54 @@ public class SnakeController : MonoBehaviour
 
             moveTimer = 0f;
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Food"))
+        {
+            HandleCollisionWithFood(other);
+
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void HandleCollisionWithFood(Collider2D colliderFoodObject)
+    {
+        if (colliderFoodObject.gameObject.GetComponent<FoodController>() != null)
+        {
+            if(colliderFoodObject.gameObject.GetComponent<FoodController>().getFoodType()==FoodType.MassBurnerFood)
+            {
+                ReduceSnakeSize();
+            }
+            else if (colliderFoodObject.gameObject.GetComponent<FoodController>().getFoodType() == FoodType.MassGainerFood)
+            {
+                GrowSnakeSize();
+            }
+        }
+
+    }
+
+
+    private void GrowSnakeSize()
+    {
+    Transform newSnakeSegment = Instantiate(this.snakeSegmentPrefab);
+    newSnakeSegment.position = snakeSegmentList[snakeSegmentList.Count - 1].position;
+
+    snakeSegmentList.Add(newSnakeSegment);
+
+    }
+
+    private void ReduceSnakeSize()
+        {
+        if (snakeSegmentList.Count > 1)
+        { 
+        Transform lastBodyPart = snakeSegmentList[snakeSegmentList.Count - 1];
+        snakeSegmentList.RemoveAt(snakeSegmentList.Count - 1);
+
+        Destroy(lastBodyPart.gameObject);
+        }
+
     }
 
 }
