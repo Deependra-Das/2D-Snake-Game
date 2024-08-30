62b42c8af6d5caf033cd96b53609228a780a04f0
Deependra-Das <iamdeep75@gmail.com>
2024-08-30 18:04:16 +0530

    Added Basic UI for SinglePlayer

---
+++ Assets/Scripts/GameUIManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameUIManager : MonoBehaviour
+{
+
+    private int scoreValue = 0;
+
+    [SerializeField]
+    private TextMeshProUGUI scoreText;
+
+    public SnakeController snakeController;
+
+    public CanvasRenderer[] ActivePowerupImageList;
+
+    public void IncreaseScore(int scoreIncrementValue)
+    {
+        scoreValue += scoreIncrementValue;
+        RefreshUI();
+    }
+
+    private void RefreshUI()
+    {
+        scoreText.text = "Score: " + scoreValue.ToString();
+
+    }
+
+    private void Update()
+    {
+        UpadateActivePowerup();
+    }
+
+    public void UpadateActivePowerup()
+    {
+        ActivePowerupImageList[0].gameObject.SetActive(snakeController.checkShieldStatus());
+        ActivePowerupImageList[1].gameObject.SetActive(snakeController.checkScoreMultiplierStatus());
+        ActivePowerupImageList[2].gameObject.SetActive(snakeController.checkSpeedBoostStatus());
+            
+    }
+}
